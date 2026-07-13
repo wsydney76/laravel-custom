a0f0ffc407607680b6d017833394b80bfadce76b
wsydn <wsydney@web.de>
2026-07-13 15:44:59 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import {
+    defineConfig
+} from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+            fonts: [
+                bunny('Instrument Sans', {
+                    weights: [400, 500, 600],
+                }),
+            ],
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        strictPort: true,
+        origin: `${process.env.DDEV_PRIMARY_URL_WITHOUT_PORT}:5173`,
+        cors: {
+            origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
+        },
+    },
+});
