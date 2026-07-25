a17df4d3acd180ac34b3a86d5b934b77404c837b
wsydn <wsydney@web.de>
2026-07-25 11:24:29 +0200

    Refactor HomeController to use Inspiring quotes and cache featured note ID

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Note;
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function show()
     {
         $title = config('app.name');
-        $teaser = fake()->sentence(12);
+        $teaser = Inspiring::quotes()->random();
         $notesCount = Note::count();
 
-        $featuredNote = Note::inRandomOrder()->first();
+        $featuredNoteId = Cache::remember('featured_note_id', now()->addDay(), function () {
+            return Note::inRandomOrder()->value('id');
+        });
+
+        $featuredNote = $featuredNoteId ? Note::find($featuredNoteId) : null;
 
         return view('home', compact(['title', 'teaser', 'notesCount', 'featuredNote']));
     }
